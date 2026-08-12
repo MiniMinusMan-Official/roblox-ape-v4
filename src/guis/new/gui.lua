0fa9352d4d2b1ff1ccc902b2cd5ea10609cdd12f
MiniMinusMan-Official <csimel+1@hotmail.com>
2026-08-12 17:08:38 +0100

    undone fix

--- src/guis/new/gui.lua
+++ src/guis/new/gui.lua
@@ -586,8 +586,8 @@ function mainapi:CreateGUI()
 	makeDraggable(window)
 	local logo = Instance.new('ImageLabel')
 	logo.Name = 'VapeLogo'
-	logo.Size = UDim2.fromOffset(48, 18)
-	logo.Position = UDim2.fromOffset(11, 10)
+	logo.Size = UDim2.fromOffset(62, 18)
+	logo.Position = UDim2.fromOffset(-5, 10)
 	logo.BackgroundTransparency = 1
 	logo.Image = getcustomasset('newvape/assets/new/guivape.png')
 	logo.ImageColor3 = select(3, uipallet.Main:ToHSV()) > 0.5 and uipallet.Text or Color3.new(1, 1, 1)
