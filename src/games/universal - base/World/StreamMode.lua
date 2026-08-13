8cb2da966b7282572711e36e107b7e1bb14b37c5
MiniMinusMan-Official <csimel+1@hotmail.com>
2026-08-13 17:33:33 +0100

    optimization

--- src/games/universal - base/World/StreamMode.lua
+++ src/games/universal - base/World/StreamMode.lua
@@ -2,7 +2,7 @@ local StreamerMode
 
 StreamerMode = vape.Categories.World:CreateModule({
 	Name = 'StreamerMode',
-	Tooltip = 'Hides your username and (optionally) your avatar too\nby the way this is a 1-time use so you have to turn it off and rejoin to undo',
+	Tooltip = 'Hides your username\nrejoin to undo, this WILL disable itself once it loads',
 	Function = function(callback)
 		if callback then
 			plr = game:GetService("Players").LocalPlayer
@@ -24,7 +24,7 @@ StreamerMode = vape.Categories.World:CreateModule({
 			local cha = e()
 
 			function changeText(n)
-			   plr.Name, plr.DisplayName = "robolx", "robolx"
+			    plr.Name, plr.DisplayName = "robolx", "robolx"
 				if n:IsA("TextLabel") or n:IsA("TextButton") or n:IsA("TextBox") then
 					if n.Text:lower():find(display:lower()) and not n.Text:find("robolx") then
 						n.Text = n.Text:lower():gsub(display:lower(), "robolx")
@@ -34,21 +34,12 @@ StreamerMode = vape.Categories.World:CreateModule({
 					end
 				end
 			end
-			task.spawn(function()
-				plr.Name, plr.DisplayName = "robolx", "robolx"
-				while task.wait(5) do
-					for i, v in next, game:GetDescendants() do
-						changeText(v)
-					end
-				end
-			end)
+			plr.Name, plr.DisplayName = "robolx", "robolx"
+			for i, v in next, game:GetDescendants() do
+				changeText(v)
+			end
 			game.DescendantAdded:Connect(changeText)
 
-			for _, part in next, cha:GetDescendants() do
-				if part:IsA("Part") then
-					part.Color = Color3.fromRGB(math.random(1, 255), math.random(1, 255), math.random(1, 255))
-				end
-			end 
 			StreamerMode.Enabled = false
 		else end
 	end
