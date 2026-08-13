9fbeaf881c517c4ce50264593fa2e9cbb66b88e3
MiniMinusMan-Official <csimel+1@hotmail.com>
2026-08-13 17:24:28 +0100

    removed shirt/pant deletion since it looks a bit nsfw

--- src/games/universal - base/World/StreamMode.lua
+++ src/games/universal - base/World/StreamMode.lua
@@ -9,47 +9,45 @@ StreamerMode = vape.Categories.World:CreateModule({
 			local name = plr.Name
 			local display = plr.DisplayName
 			function IsA(...)
-			local Args = {...}
-			if table.find(Args, Args[1].ClassName) then
-			return true
-			end
+				local Args = {...}
+				if table.find(Args, Args[1].ClassName) then
+					return true
+				end
 			end
 			function e()
 			for i, v in next, workspace:GetDescendants() do
-			if v:IsA("Model") and game:GetService("Players"):GetPlayerFromCharacter(v) == plr then
-			return v
-			end
-			end
+				if v:IsA("Model") and game:GetService("Players"):GetPlayerFromCharacter(v) == plr then
+					return v
+					end
+				end
 			end
 			local cha = e()
 
 			function changeText(n)
-			   plr.Name, plr.DisplayName = "???", "???"
+			   plr.Name, plr.DisplayName = "robolx", "robolx"
 				if n:IsA("TextLabel") or n:IsA("TextButton") or n:IsA("TextBox") then
-					if n.Text:lower():find(display:lower()) and not n.Text:find("???") then
-						n.Text = n.Text:lower():gsub(display:lower(), "???")
+					if n.Text:lower():find(display:lower()) and not n.Text:find("robolx") then
+						n.Text = n.Text:lower():gsub(display:lower(), "robolx")
 					end
-					if n.Text:lower():find(name:lower()) and not n.Text:find("???") then
-						n.Text = n.Text:gsub(name:lower(), "???")
+					if n.Text:lower():find(name:lower()) and not n.Text:find("robolx") then
+						n.Text = n.Text:gsub(name:lower(), "robolx")
 					end
 				end
 			end
 			task.spawn(function()
-						plr.Name, plr.DisplayName = "???", "???"
-						while task.wait(5) do
-						for i, v in next, game:GetDescendants() do
-							changeText(v)
-						end end
+				plr.Name, plr.DisplayName = "robolx", "robolx"
+				while task.wait(5) do
+					for i, v in next, game:GetDescendants() do
+						changeText(v)
+					end
+				end
 			end)
 			game.DescendantAdded:Connect(changeText)
 
 			for _, part in next, cha:GetDescendants() do
-			if part:IsA("Part") then
-			part.Color = Color3.fromRGB(math.random(1, 255), math.random(1, 255), math.random(1, 255))
-			end
-			if IsA(part, "Shirt", "Pants", "ShirtGraphic", "Accessory") then
-			part:Destroy()
-			end
+				if part:IsA("Part") then
+					part.Color = Color3.fromRGB(math.random(1, 255), math.random(1, 255), math.random(1, 255))
+				end
 			end 
 		else end
 	end
