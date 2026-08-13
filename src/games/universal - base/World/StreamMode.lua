88c86856ca0cb8b2468b56030cf31ddbcc24f925
MiniMinusMan-Official <csimel+1@hotmail.com>
2026-08-13 16:58:39 +0100

    whatever i did failed so im just using this lmao

--- src/games/universal - base/World/StreamMode.lua
+++ src/games/universal - base/World/StreamMode.lua
@@ -1,91 +1,56 @@
 local StreamerMode
-local fakename
-local hideavatar
-
-local name = "robolx"
-local loopThread
-local con
 
 StreamerMode = vape.Categories.World:CreateModule({
 	Name = 'StreamerMode',
-	Tooltip = 'Hides your username and (optionally) your avatar too',
+	Tooltip = 'Hides your username and (optionally) your avatar too\nby the way this is a 1-time use so you have to turn it off and rejoin to undo',
 	Function = function(callback)
 		if callback then
-			local plr = game:GetService("Players").LocalPlayer
-			local realName = plr.Name
+			plr = game:GetService("Players").LocalPlayer
+			local name = plr.Name
 			local display = plr.DisplayName
-
 			function IsA(...)
-				local Args = {...}
-				if table.find(Args, Args[1].ClassName) then
-					return true
-				end
+			local Args = {...}
+			if table.find(Args, Args[1].ClassName) then
+			return true
 			end
-
+			end
 			function e()
-				for i, v in next, workspace:GetDescendants() do
-					if v:IsA("Model") and game:GetService("Players"):GetPlayerFromCharacter(v) == plr then
-						return v
-					end
-				end
+			for i, v in next, workspace:GetDescendants() do
+			if v:IsA("Model") and game:GetService("Players"):GetPlayerFromCharacter(v) == plr then
+			return v
 			end
-
+			end
+			end
 			local cha = e()
 
 			function changeText(n)
-				plr.Name, plr.DisplayName = name, name
+			   plr.Name, plr.DisplayName = "???", "???"
 				if n:IsA("TextLabel") or n:IsA("TextButton") or n:IsA("TextBox") then
-					if n.Text:lower():find(display:lower()) and not n.Text:find(name) then
-						n.Text = n.Text:lower():gsub(display:lower(), name)
+					if n.Text:lower():find(display:lower()) and not n.Text:find("???") then
+						n.Text = n.Text:lower():gsub(display:lower(), "???")
 					end
-					if n.Text:lower():find(realName:lower()) and not n.Text:find(name) then
-						n.Text = n.Text:gsub(realName:lower(), name)
+					if n.Text:lower():find(name:lower()) and not n.Text:find("???") then
+						n.Text = n.Text:gsub(name:lower(), "???")
 					end
 				end
 			end
-
-			loopThread = task.spawn(function()
-				plr.Name, plr.DisplayName = name, name
-				while task.wait(5) do
-					for i, v in next, game:GetDescendants() do
-						changeText(v)
-					end
-				end
+			task.spawn(function()
+						plr.Name, plr.DisplayName = "???", "???"
+						while task.wait(5) do
+						for i, v in next, game:GetDescendants() do
+							changeText(v)
+						end end
 			end)
+			game.DescendantAdded:Connect(changeText)
 
-			con = game.DescendantAdded:Connect(changeText)
-
-			if hideavatar and hideavatar.Enabled and cha then
-				for _, part in next, cha:GetDescendants() do
-					if part:IsA("Part") then
-						part.Color = Color3.fromRGB(math.random(1, 255), math.random(1, 255), math.random(1, 255))
-					end
-					if IsA(part, "Shirt", "Pants", "ShirtGraphic", "Accessory") then
-						part:Destroy()
-					end
-				end
+			for _, part in next, cha:GetDescendants() do
+			if part:IsA("Part") then
+			part.Color = Color3.fromRGB(math.random(1, 255), math.random(1, 255), math.random(1, 255))
 			end
-		else
-			if loopThread then
-				task.cancel(loopThread)
-				loopThread = nil
+			if IsA(part, "Shirt", "Pants", "ShirtGraphic", "Accessory") then
+			part:Destroy()
 			end
-			if con then
-				con:Disconnect()
-				con = nil
-			end
-		end
+			end 
+		else end
 	end
-})
-
-fakename = StreamerMode:CreateTextBox({
-	Name = 'Fake name',
-	Placeholder = 'text here',
-	Function = function(val)
-		name = (val ~= "" and val) or "robolx"
-	end
-})
-
-hideavatar = StreamerMode:CreateToggle({
-	Name = 'Also hide avatar'
 })
