d29f1884a6b95974db49e85f20320fd7e8eb2a86
MiniMinusMan-Official <csimel+1@hotmail.com>
2026-08-13 11:54:59 +0100

    Update FOV.lua

--- src/games/universal - base/Legit/FOV.lua
+++ src/games/universal - base/Legit/FOV.lua
@@ -1,17 +1,30 @@
 local FOV
 local Value
 local oldfov
+local fovconnection
 
 FOV = vape.Legit:CreateModule({
 	Name = 'FOV',
 	Function = function(callback)
 		if callback then
 			oldfov = gameCamera.FieldOfView
+
+			fovconnection = gameCamera:GetPropertyChangedSignal('FieldOfView'):Connect(function()
+				if FOV.Enabled and gameCamera.FieldOfView ~= Value.Value then
+					gameCamera.FieldOfView = Value.Value
+				end
+			end)
+
 			repeat
 				gameCamera.FieldOfView = Value.Value
-				task.wait()
+				game:GetService('RunService').RenderStepped:Wait()
 			until not FOV.Enabled
 		else
+			if fovconnection then
+				fovconnection:Disconnect()
+				fovconnection = nil
+			end
+
 			gameCamera.FieldOfView = oldfov
 		end
 	end,
