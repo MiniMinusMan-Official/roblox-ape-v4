1e13b52f41be07261c90c930bc8461e2db207c23
MiniMinusMan-Official <csimel+1@hotmail.com>
2026-08-14 21:27:54 +0100

    Create scp_RapidFire.lua

---
+++ src/games/universal - base/Combat/scp_RapidFire.lua
@@ -0,0 +1,54 @@
+local RapidFire
+local FRate
+local OriginalValues = {}
+
+local function modifyTBS(value, isReverting)
+    for _, module in pairs(getloadedmodules()) do
+        local success, result = pcall(require, module)
+        if success and type(result) == "table" then
+            pcall(function()
+                if rawget(result, "Pistol") ~= true then
+                    if rawget(result, "TBS") ~= nil then
+                        if isReverting then
+                            result.TBS = OriginalValues[module] or 0.2
+                        else
+                            if not OriginalValues[module] then
+                                OriginalValues[module] = result.TBS
+                            end
+                            result.TBS = value
+                        end
+                    end
+                end
+            end)
+        end
+    end
+end
+
+local inSCPRP = game.PlaceId == 5041144419 or game.PlaceId == 10953555034
+
+if inSCPRP then
+	RapidFire = vape.Categories.Combat:CreateModule({
+		Name = 'Rapid Fire',
+		Function = function(callback)
+			if callback then
+				task.spawn(function()
+					while RapidFire.Enabled do
+						modifyTBS(FRate.Value, false)
+						task.wait(1)
+					end
+				end)
+			else
+				modifyTBS(nil, true)
+				OriginalValues = {}
+			end
+		end,
+		Tooltip = 'changes the fire rate of weapons'
+	})
+
+	FRate = RapidFire:CreateSlider({
+		Name = 'Fire rate',
+		Min = 0.05,
+		Max = 0.2,
+		Decimal = 100
+	})
+end
